f61f738beff9a2d1c19e6d2cc16936f530553033
ferkomedia <info@ferkomedia.sk>
2026-01-10 23:41:00 +0100

    Fix sitemap site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  site: "https://portretnefilmy.sk",
+  output: "static",
+  integrations: [sitemap()],
 });
-
