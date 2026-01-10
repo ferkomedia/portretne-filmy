01d0958d9b91c6f04e2d640dac77caaf4ee4fcdd
ferkomedia <info@ferkomedia.sk>
2026-01-10 23:49:07 +0100

    Fix sitemap for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://portretnefilmy.sk",
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/404"),
+    }),
+  ],
   output: "static",
-  integrations: [sitemap()],
 });
