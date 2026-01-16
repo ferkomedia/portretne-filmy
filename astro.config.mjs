7c4438d5f4860cbea3bced9b545f010bbd4735de
ferkomedia <info@ferkomedia.sk>
2026-01-16 01:00:33 +0100

    SEO: Update OG image with brand colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: "https://portretnefilmy.sk",
-  integrations: [
-    sitemap({
-      filter: (page) => !page.includes("/404"),
-    }),
-  ],
-  output: "static",
+  site: 'https://portretnefilmy.sk',
+  integrations: [sitemap()]
 });
